1c105bca969f7e9dc2c1b01768bde58934f07a3f
Vivek <vk0956709@gmail.com>
2025-11-02 12:24:02 +0530

    fix: update todo update logic

--- app.py
+++ app.py
@@ -4,56 +4,65 @@ import os
 
 app = Flask(__name__)
 
+# ✅ DB Path
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# ---- Model ----
+# ✅ Model
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     complete = db.Column(db.Boolean, default=False)
 
-# ---- Create new database file ----
+# ✅ Create DB
 with app.app_context():
     db.create_all()
-    print("✅ New database created at:", os.path.join(basedir, 'db.sqlite'))
 
+# ✅ Get all Tasks
 @app.route('/')
 def index():
     todo_list = Todo.query.all()
     return render_template('base.html', todo_list=todo_list)
 
-# def add():
-#     title = request.form.get("title")
-#     new_todo = Todo(title=title, complete=False)
-#     db.session.add(new_todo)
-#     db.session.commit()
-#     return redirect(url_for())
-
+# ✅ Add Task
 @app.route('/add', methods=['POST'])
 def add():
     title = request.form.get('title')
     if title:
         new_todo = Todo(title=title)
         db.session.add(new_todo)
-        db.session.commit()
+        try:
+            db.session.commit()
+        except Exception as e:
+            print("database error", e)
+            db.session.rollback()
     return redirect(url_for('index'))
 
+# ✅ Update Task
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
+    todo = Todo.query.get(todo_id)
     todo.complete = not todo.complete
-    db.session.commit()
+    try:
+        db.session.commit()
+    except Exception as e:
+        print("database error", e)
+        db.session.rollback()
     return redirect(url_for('index'))
 
+# ✅ Delete Task
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
+    todo = Todo.query.get(todo_id)
     db.session.delete(todo)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except Exception as e:
+        print("database error", e)
+        db.session.rollback()
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
